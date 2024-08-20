39bcdb619b0f875d5b32eac3ce5145c405bf56c0
Rafael <rafael@example.com>
2024-08-19 22:41:59 -0300

    Installing typescript into project

---
+++ app.ts
@@ -0,0 +1,5 @@
+const nome: String = "Rafael";
+const idade: Number = 10;
+const verdadeiro: Boolean = true;
+
+console.log(nome, idade, verdadeiro);
---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const nome = "Rafael";
+const idade = 10;
+const verdadeiro = true;
+console.log(nome, idade, verdadeiro);
